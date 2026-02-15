65aa8aab99370a397cc6910c3b260b7266a8c739
Guthib-of-Dan <diril656@gmail.com>
2026-02-15 10:08:31 -0800

    Updated architecture to reduce configuration time and support multiple 'exports' from package while running tests

--- tests/all.ts
+++ tests/all.ts
@@ -1,12 +1,36 @@
-import {expect, test} from "vitest"
-import {expectType} from "tsd"
+import {expect, test, describe} from "vitest"
+import {expectType, expectError} from "tsd"
 var runningTsd: boolean = false;
 
-export default function(module: typeof import("mylib")){
-  test("hello", ()=>{
-    expect(module.hello(true)).toBe(true)
-    if(runningTsd){
-      expectType<true>(module.hello(true))
-    }
+
+// here if we write "typeof import" we test package.json (when write - "exports", when use tsd - "typesVersion") 
+export function testIndexModule(module: typeof import("mylib")){
+  describe("index module testing", () => {
+    test("hello", () => {
+      expect(module.hello(true)).toBe(true)
+      if (runningTsd) {
+        expectType<true>(module.hello(true))
+      }
+    })
+  })
+}
+
+export function testSubModule(module: typeof import("mylib/sub")){
+  describe("sub module testing", ()=>{
+    test("hasPermission", () => {
+      var permission = module.hasPermission("admin", "private");
+      expect(permission).toBe(true);
+      expect(module.getDataByPermission(permission)).toEqual({})
+      if (runningTsd) {
+        expectType<false>(module.hasPermission("user", "private"))
+        expectType<true>(module.hasPermission("user", "public"))
+        expectType<true>(module.hasPermission("admin", "private"))
+        expectType<{}>(module.getDataByPermission(true))
+        expectType<null>(module.getDataByPermission(false))
+        expectError(module.hasPermission("elite", "private"))
+        expectError(module.hasPermission("admin", "external"))
+      }
+    })
+
   })
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,8 +1,24 @@
-import runTests from "./all"
+import runTests from "./abstraction"
 import {createRequire} from "node:module"
-import {describe} from "vitest"
-var require = createRequire(import.meta.url)
-import * as esm from "mylib"
-var cjs: any = require("mylib")
-describe("ESM", ()=>runTests(esm))
-describe("CJS", ()=>runTests(cjs))
+var require = createRequire(import.meta.url);
+
+
+await runTests(/*External dependencies not to be bundled with esbuild*/ ["uWebSockets.js"], [
+  {
+    // test "exports" field in package.json when running vitest
+    normalCJS: require("mylib"),
+    // test "exports" field in package.json when running vitest
+    normalESM: await import("mylib"),
+    // name of testing function from "all.ts"
+    test: "testIndexModule",
+    // name of file in 'src' with .js extension to be minified and bundled 
+    name: "index.js"
+  },
+  // several entries
+  {
+    normalCJS: require("mylib/sub"),
+    normalESM: await import("mylib/sub"),
+    test: "testSubModule",
+    name: "sub.js"
+  }
+])
